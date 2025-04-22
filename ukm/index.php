7f6b893574e43395f3f2699c1d50318c742fc4dc
Ahmad Farisi <ahmfarisi@gmail.com>
2025-04-22 12:46:35 +0700

    Update index.php

---
+++ ukm/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UKM</title>
+</head>
+<body>
+    <h1>Form Pendaftaran UKM</h1>
+</body>
+</html>
