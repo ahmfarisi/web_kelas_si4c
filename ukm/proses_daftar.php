cee3673748e93812eeb8b070e6137ff12d98ffc3
Ahmad Farisi <ahmfarisi@gmail.com>
2025-04-29 12:20:29 +0700

    Update proses_daftar.php

--- ukm/proses_daftar.php
+++ ukm/proses_daftar.php
@@ -4,12 +4,54 @@
     $tangkap_npm = $_POST["var_npm"];
     $tangkap_nama = $_POST["var_nama"];
     $tangkap_ukm = $_POST["var_ukm"];
-
-    // Tampilin yang udah ditangkep pake echo atau print
-    echo $tangkap_npm;
-    echo "<br/>";
-    echo $tangkap_nama;
-    echo "<br/>";
-    echo $tangkap_ukm;
-    echo "<br/>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UKM</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container mt-4">
+
+        <div class="card">
+            <div class="card-header"><strong>Form Pendaftaran UKM</strong></div>
+
+                <div class="card-body">
+                
+                        <div class="mb-3">
+                            <label for="var_npm" class="form-label">NPM:</label>
+                            <label for="var_npm" class="form-label"><?php echo $tangkap_npm; ?></label>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="var_nama" class="form-label">Nama:</label>
+                            <label for="var_nama" class="form-label"><?php echo $tangkap_nama; ?></label>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="var_ukm" class="form-label">Pilihan UKM:</label>
+                            <label for="var_ukm" class="form-label"><?php echo $tangkap_ukm; ?></label>
+                        </div>
+
+                </div>
+
+                <div class="card-footer">
+                    <a href="index.php" class="btn btn-primary">Kembali</a>
+                </div>
+
+        </div>
+
+    </div>
+
+
+</body>
+</html>
