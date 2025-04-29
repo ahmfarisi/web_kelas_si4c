504d74ef8b1056b26b09ea2e9999eb66f31d48e7
Ahmad Farisi <ahmfarisi@gmail.com>
2025-04-29 13:11:42 +0700

    Update koneksi.php

--- pendaftaran_ukm/koneksi.php
+++ pendaftaran_ukm/koneksi.php
@@ -4,4 +4,4 @@ $user = "root";
 $pass = "";
 $dbname = "db_ukm";
 
-$connect = mysqli_connect($host, $user, $pass, $dbname) or die('Database Tidak Terhubung');
+$conn = mysqli_connect($host, $user, $pass, $dbname) or die('Database Tidak Terhubung');
