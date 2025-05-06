479d49a8e108bbd9ee426c0e202170319fb0a817
Ahmad Farisi <ahmfarisi@gmail.com>
2025-05-06 09:37:42 +0700

    First Step CRUD Basic PHP MySQL

--- pendaftaran_ukm/koneksi.php
+++ pendaftaran_ukm/koneksi.php
@@ -5,3 +5,9 @@ $pass = "";
 $dbname = "db_ukm";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname) or die('Database Tidak Terhubung');
+
+// Check connection
+if ($conn -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn -> connect_error;
+    exit();
+  }
